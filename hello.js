d28f936ebd15c1ff878684c001ef6985a753d8b5
PeterMaina <peterwahomemaina003@gmail.com>
2025-01-23 20:42:23 +0300

    hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 console.log('2025');
 
-const goals = ["Healthier body","Wealthier mind", "Walthier pockets"];
+const goals = ["Healthier body","Wealthier mind", "Weathier pockets"];
+
+Document.getElementbyId("parser").InnerHTML(goals);
